dd02b12891dee96782df25681c5f03ddcbde8f5f
trader-daniil <74257219+trader-daniil@users.noreply.github.com>
2024-04-13 17:18:32 +0300

    correct Readme

--- deploy_star_burger.sh
+++ deploy_star_burger.sh
@@ -3,7 +3,7 @@ git pull
 sudo systemctl status nginx
 sudo systemctl status star-burger
 /opt/star_burger/venv/bin/pip3 install -r requirements.txt
-npm ci --dev
+npm install
 ./node_modules/.bin/parcel build bundles-src/index.js --dist-dir bundles --public-url="./"
 /opt/star_burger/venv/bin/python3 manage.py migrate
 /opt/star_burger/venv/bin/python3 manage.py collectstatic
