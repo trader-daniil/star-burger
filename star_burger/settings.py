657d438d3062080b738b3d6b80e7b0b755ea4c22
trader-daniil <74257219+trader-daniil@users.noreply.github.com>
2024-04-17 19:16:12 +0300

    remove comment

--- star_burger/settings.py
+++ star_burger/settings.py
@@ -20,7 +20,6 @@ GEOCODE_URL = 'https://geocode-maps.yandex.ru/1.x'
 ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', ['127.0.0.1', 'localhost'])
 
 INSTALLED_APPS = [
-    #'foodcartapp.apps.FoodcartappConfig',
     'restaurateur.apps.RestaurateurConfig',
     'django.contrib.admin',
     'django.contrib.auth',
