3d3f99ab04e8f93141f6525d089eb6d4f14b2900
trader-daniil <74257219+trader-daniil@users.noreply.github.com>
2024-04-17 19:36:31 +0300

    add yes to deploy script

--- deploy_star_burger.sh
+++ deploy_star_burger.sh
@@ -4,8 +4,8 @@ export $(grep -v '^#' .env | xargs)
 /opt/star_burger/venv/bin/pip3 install -r requirements.txt
 npm install
 ./node_modules/.bin/parcel build bundles-src/index.js --dist-dir bundles --public-url="./"
-/opt/star_burger/venv/bin/python3 manage.py migrate
-/opt/star_burger/venv/bin/python3 manage.py collectstatic
+/opt/star_burger/venv/bin/python3 manage.py migrate -y
+/opt/star_burger/venv/bin/python3 manage.py collectstatic -y
 sudo systemctl restart nginx
 sudo systemctl restart star-burger
 gitrev=$(git rev-parse HEAD)
