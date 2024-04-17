44142e11496b3227a04c1d5aeda4cc616c16fed6
trader-daniil <74257219+trader-daniil@users.noreply.github.com>
2024-04-17 19:38:59 +0300

    change deploy script

--- deploy_star_burger.sh
+++ deploy_star_burger.sh
@@ -4,7 +4,7 @@ export $(grep -v '^#' .env | xargs)
 /opt/star_burger/venv/bin/pip3 install -r requirements.txt
 npm install
 ./node_modules/.bin/parcel build bundles-src/index.js --dist-dir bundles --public-url="./"
-/opt/star_burger/venv/bin/python3 manage.py migrate -y
+/opt/star_burger/venv/bin/python3 manage.py migrate
 /opt/star_burger/venv/bin/python3 manage.py collectstatic -y
 sudo systemctl restart nginx
 sudo systemctl restart star-burger
