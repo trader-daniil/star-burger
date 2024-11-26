2e05d51c5b0fc8c47f489734e151208456468335
trader-daniil <74257219+trader-daniil@users.noreply.github.com>
2024-11-26 12:23:51 +0300

    add docker compose frontend

--- star_burger/settings.py
+++ star_burger/settings.py
@@ -135,7 +135,7 @@ INTERNAL_IPS = [
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "assets"),
-    os.path.join(BASE_DIR, "bundles"),
+    os.path.join(BASE_DIR, "frontend/bundles"),
 ]
 
 PHONENUMBER_DEFAULT_REGION = 'RU'
